33db3d56b2a0320282fb8e34566c9539d121ac4a
jjangjjh2222 <jjangjjh2222@naver.com>
2022-08-10 20:21:17 +0900

    Docs : SECRET_KEY 숨김

--- back/movieProject/movieProject/settings.py
+++ back/movieProject/movieProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import datetime
+from secretKey import SECRET_KEY as KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$&gt)@3-lbf7y(#h=4^pf9d2xxyzi*b6=yp#(60+^-p)84$#v('
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
