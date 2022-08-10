dce4b7d00abae76dffd69d41b0c972b867823b62
jjangjjh2222 <jjangjjh2222@naver.com>
2022-08-10 20:06:07 +0900

    Refactor : Movie List api에 이미지 업로드 추가

--- back/movieProject/accounts/models.py
+++ back/movieProject/accounts/models.py
@@ -54,6 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # 사용자의 username field는 nickname으로 설정
     USERNAME_FIELD = 'nickname'
+    
     # 필수로 작성해야하는 field
     # REQUIRED_FIELDS = ['email', 'name']
 
--- back/movieProject/movieApp/models.py
+++ back/movieProject/movieApp/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=30) # 제목
-    genre = models.CharField(max_length=15) # 장르
-    year = models.IntegerField() # 제작 년도
+    image = models.ImageField(blank=True, null=True, upload_to="uploads")
 
     def __str__(self):
         return self.title
