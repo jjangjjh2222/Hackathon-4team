d4ae637b231930d1bd2434b744e58d220f657b5e
chweyun <chweyun>
2022-08-12 00:14:39 +0900

    채팅기능 구현

--- front/src/pages/Chat.jsx
+++ front/src/pages/Chat.jsx
@@ -3,22 +3,70 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Chat = () => {
+
+    const roomName = JSON.parse(document.getElementById('room-name'));
+
+        const chatSocket = new WebSocket(
+            'ws://'
+            + window.location.host
+            + '/ws/chat/'
+            + roomName
+            + '/'
+        );
+
+        chatSocket.onmessage = function(e) {
+            const data = JSON.parse(e.data);
+            document.querySelector('#chat-log').value += (data.message + '\n');
+        };
+
+        chatSocket.onclose = function(e) {
+            console.error('Chat socket closed unexpectedly');
+        };
+
+        // document.querySelector('#chat-message-input').focus();
+        // document.querySelector('#chat-message-input').onkeyup = function(e) {
+        //     if (e.keyCode === 13) {  // enter, return
+        //         document.querySelector('#chat-message-submit').click();
+        //     }
+        // };
+
+        // document.querySelector('#chat-message-submit').onclick = function(e) {
+        //     const messageInputDom = document.querySelector('#chat-message-input');
+        //     const message = messageInputDom.value;
+        //     chatSocket.send(JSON.stringify({
+        //         'message': message
+        //     }));
+        //     messageInputDom.value = '';
+        // };
     
+
+        const submitBtn = (e) => {
+            // const messageInputDom = document.querySelector('#chat-message-input');
+            const messageInputDom = document.getElementById('chat-message-input');
+            const message = messageInputDom.value;
+            chatSocket.send(JSON.stringify({
+                        'message': message
+                    }));
+                    messageInputDom.value = '';
+        }
+
+
     return (
         <>
             <TopBox>
-                <Title>이상한 변호사 우영우</Title>
-                {/* TODO */}
+                {/* TODO roomName 상수로 변경 */}
+                <Title>이상한 변호사 우영우</Title> 
+                {/* TODO 시간에 따라 점점 움직이는 바 넣기 */}
                 <DivTime>시간에 따라 점점 움직이는 바 넣기</DivTime>
             </TopBox>
-            <ChatArea>
+            <ChatArea id="chat-log" cols="100" rows="20">
             </ChatArea>
             <BtmBox>
                 <Div1>
                     <Div2>
-                        <Input></Input>
+                        <Input id="chat-message-input" type="text" size="100"></Input>
                         <Div3>
-                        <Button style={{borderStyle:"none"}}>전송</Button>
+                        <Button onClick={submitBtn} id="chat-message-submit" type="button" value="전송" style={{borderStyle:"none"}}></Button>
                         </Div3>
                     </Div2>
                 </Div1>
@@ -34,8 +82,8 @@ const TopBox = styled.div`
     width: 100%;
     height: 140px;
     padding-top: 40px;
-    // background-color: #2c3333;
-    background-color: red;
+    background-color: #2c3333;
+    // background-color: red;
     position: fixed;
     top: 0;
     left: 0;
@@ -51,7 +99,7 @@ const Title = styled.h1`
 `
 
 const DivTime = styled.div`
-    background-color: yellow;
+    // background-color: yellow;
     height: 50%;
     width: 80%;
     margin: auto;
@@ -59,9 +107,11 @@ const DivTime = styled.div`
 
 `
 
-const ChatArea = styled.div`
+const ChatArea = styled.textarea`
     margin-top: 180px;
     padding-bottom: 80px;
+    width: 100%;
+    height: 60vh;
 `
 
 const P = styled.p`
@@ -109,7 +159,7 @@ const Div3 = styled.div`
     float: right;
 `
 
-const Button = styled.button`
+const Button = styled.input`
     width: 100%;
     height: 100%;
     font-size: 18px;
