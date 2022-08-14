a89c80e7827fd95b3cf0095a8fc3675e6d6fea80
jjangjjh2222 <jjangjjh2222@naver.com>
2022-08-15 02:03:21 +0900

    refactor: 쓰레기 데이터 삭제

--- back/movieProject/chat/models.py
+++ back/movieProject/chat/models.py
@@ -4,12 +4,12 @@ from accounts.models import User
 from movieApp.models import Movie
 
 
-class Chat(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    content = models.CharField(max_length=200)
-    on_created = models.DateTimeField(auto_now_add=True)
-    movie = models.ForeignKey(Movie, blank=True, null=True, on_delete = models.CASCADE)
+# class Chat(models.Model):
+#     user = models.ForeignKey(User, on_delete = models.CASCADE)
+#     content = models.CharField(max_length=200)
+#     on_created = models.DateTimeField(auto_now_add=True)
+#     movie = models.ForeignKey(Movie, blank=True, null=True, on_delete = models.CASCADE)
     
 
-    def __str__(self):
-        return self.user.nickname + " " + self.content + " " + localize(self.on_created)
+#     def __str__(self):
+#         return self.user.nickname + " " + self.content + " " + localize(self.on_created)
